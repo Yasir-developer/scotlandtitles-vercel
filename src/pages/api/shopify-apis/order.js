5b0a178fecc44bccdc56a9f8916bc973d55dc02b
Ali Raza Farishta <yasirupworkdev@gmail.com>
2023-08-30 16:39:20 +0500

    Update order.js

--- src/pages/api/shopify-apis/order.js
+++ src/pages/api/shopify-apis/order.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   console.log("req.body", req.body);
   //   res.setHeader("Cache-Control", "s-maxage=600, stale-while-revalidate=30"); // set caching header
-
+  console.time();
   return res.status(200);
 }
