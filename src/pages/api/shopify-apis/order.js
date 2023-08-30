00b3a4233c9e16eb749d951054a1938e4dd54940
Ali Raza Farishta <yasirupworkdev@gmail.com>
2023-08-30 18:12:30 +0500

    Update order.js

--- src/pages/api/shopify-apis/order.js
+++ src/pages/api/shopify-apis/order.js
@@ -6,30 +6,34 @@ export default async function handler(req, res) {
   //   console.time();
   //   return res.status(200);
 
-  //   try {
-  //     const pdfDoc = await PDFDocument.create();
-  //     const page = pdfDoc.addPage([600, 400]);
+  try {
+    const pdfDoc = await PDFDocument.create();
+    const page = pdfDoc.addPage([600, 400]);
 
-  //     const content = "Hello from Shopify!"; // Replace with your Shopify data
+    const content = "Hello from Shopify!"; // Replace with your Shopify data
 
-  //     page.drawText(content, {
-  //       x: 50,
-  //       y: 350,
-  //       size: 12,
-  //       color: rgb(0, 0, 0),
-  //     });
+    page.drawText(content, {
+      x: 50,
+      y: 350,
+      size: 12,
+      //   color: rgb(0, 0, 0),
+    });
 
-  //     const pdfBytes = await pdfDoc.save();
-
-  //     res.setHeader("Content-Type", "application/pdf");
-  //     res.setHeader(
-  //       "Content-Disposition",
-  //       "attachment; filename=shopify_data.pdf"
-  //     );
-  //     res.send(pdfBytes);
-  //   } catch (error) {
-  //     console.error(error);
-  //     res.status(500).send("An error occurred");
-  //   }
-  //   res.status(200).send("Successfull request");
+    const pdfBytes = await pdfDoc.save();
+    const pdfFilePath = "pdfs/pdf.pdf";
+    fs.writeFileSync(pdfFilePath, pdfBytes);
+    // res.setHeader("Content-Type", "application/pdf");
+    // res.setHeader(
+    //   "Content-Disposition",
+    //   "attachment; filename=shopify_data.pdf"
+    // );
+    const pdfUrl = `https://scotlandtitles-vercel.vercel.app/${pdfFilePath}`;
+    console.log(pdfUrl, "pdfUrl");
+    res.status(200).json({ pdfUrl });
+    // res.send(pdfBytes);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred");
+  }
+  res.status(200).send("Successfull request");
 }
