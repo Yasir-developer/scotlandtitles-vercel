f2878b93dd22783c0319116d52f14ffe8ee4d989
Ali Raza Farishta <yasirupworkdev@gmail.com>
2023-08-30 17:09:13 +0500

    Update order.js

--- src/pages/api/shopify-apis/order.js
+++ src/pages/api/shopify-apis/order.js
@@ -6,29 +6,30 @@ export default async function handler(req, res) {
   //   console.time();
   //   return res.status(200);
 
-  try {
-    const pdfDoc = await PDFDocument.create();
-    const page = pdfDoc.addPage([600, 400]);
+  //   try {
+  //     const pdfDoc = await PDFDocument.create();
+  //     const page = pdfDoc.addPage([600, 400]);
 
-    const content = "Hello from Shopify!"; // Replace with your Shopify data
+  //     const content = "Hello from Shopify!"; // Replace with your Shopify data
 
-    page.drawText(content, {
-      x: 50,
-      y: 350,
-      size: 12,
-      color: rgb(0, 0, 0),
-    });
+  //     page.drawText(content, {
+  //       x: 50,
+  //       y: 350,
+  //       size: 12,
+  //       color: rgb(0, 0, 0),
+  //     });
 
-    const pdfBytes = await pdfDoc.save();
+  //     const pdfBytes = await pdfDoc.save();
 
-    res.setHeader("Content-Type", "application/pdf");
-    res.setHeader(
-      "Content-Disposition",
-      "attachment; filename=shopify_data.pdf"
-    );
-    res.send(pdfBytes);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("An error occurred");
-  }
+  //     res.setHeader("Content-Type", "application/pdf");
+  //     res.setHeader(
+  //       "Content-Disposition",
+  //       "attachment; filename=shopify_data.pdf"
+  //     );
+  //     res.send(pdfBytes);
+  //   } catch (error) {
+  //     console.error(error);
+  //     res.status(500).send("An error occurred");
+  //   }
+  res.status(200).send("Successfull request");
 }
