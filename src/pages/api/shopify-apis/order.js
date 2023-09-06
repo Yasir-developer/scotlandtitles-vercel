59ccf104deb7d864b2a5d51452edb2c9b99bf6a4
Yasir Iqbal <yasirupworkdev@gmail.com>
2023-09-07 00:35:57 +0500

    Certificate pdf design complete - deed in progress

--- src/pages/api/shopify-apis/order.js
+++ src/pages/api/shopify-apis/order.js
@@ -76,26 +76,13 @@ export default async function handler(req, res) {
       //   console.log(pages, "pages");
       //   return;
       const page = pdfDoc.addPage([600, 800]);
-      const pagetwo = pdfDoc.addPage([800, 600]);
+      const pagetwo = pdfDoc.addPage([900, 550]);
+      const deedPage = pdfDoc.addPage([600, 800]);
+
       const embeddedPage = await pdfDoc.embedPage(page);
       const embeddedPagetwo = await pdfDoc.embedPage(pagetwo);
 
       const courierBoldFont = await pdfDoc.embedFont(StandardFonts.Courier);
-      //For background Color
-      // page.drawRectangle({
-      //   color: rgb(0, 248, 220),
-      //   width: page.getWidth(),
-      //   height: page.getHeight(),
-      // });
-      // page.drawPage(embeddedPage);
-      // page.setBackgroundColor(rgb()); // Set background color to yellow
-      // const { width, height } = page.getSize();
-      // page.drawRectangle({
-      //   color: rgb(0.25, 0.52, 0.96),
-      //   width: page.getWidth(),
-      //   height: page.getHeight(),
-      // });
-      // const pngImage = await pdfDoc.embedPng(pngImageBytes)
 
       const filePath = path.resolve("./public", "images", "scotland_log.png");
       const fontBytes = fs.readFileSync(
@@ -116,22 +103,24 @@ export default async function handler(req, res) {
 
       //   const content = "Hello from Shopify!"; // Replace with your Shopify data
 
-      req.body.line_items.map((item) => {
-        console.log(item, "itemmm");
-        // var product = item;
-        item.properties.map((propItem) => {
-          if (propItem.name == "_Name1") {
-            name1 = propItem.value;
-          }
-          if (propItem.name == "_Date") {
-            pdfDate = propItem.value;
-          }
-          console.log(propItem, "propItem");
-        });
-      });
+      // req.body.line_items.map((item) => {
+      //   console.log(item, "itemmm");
+      //   // var product = item;
+      //   item.properties.map((propItem) => {
+      //     if (propItem.name == "_Name1") {
+      //       name1 = propItem.value;
+      //     }
+      //     if (propItem.name == "_Date") {
+      //       pdfDate = propItem.value;
+      //     }
+      //     console.log(propItem, "propItem");
+      //   });
+      // });
 
-      const date = pdfDate;
-      const heading = `Land with reference number ${id}: Lord ${name1} of\nBlairadam`;
+      // const date = pdfDate;
+      const date = "22-5-2024";
+
+      const heading = `Land with reference number 321323223 Lord Craig of\nBlairadam`;
       const content = `Please find enclosed your Certificate of Disposition and Proclamation confirming you now own Land\nwithin a Scottish Estate . You may choose to adopt the traditional Scottish title of Laird as a sign of\nrespect, or the English language equivalent.\n\nYour land is located within our Estate with street address of Kingseat Road (off Cantsdam Road),\nCantsdam, Kelty, Fife, Scotland KY12 0SW. Your plot of land is located beside Kingseat Road single\ntrack road that leads north from the B912 Cantsdam Road.\n\nYou can view the land online. The following coordinates will show you the centre of the Estate;\n\nGoogle Maps type in  coordinates 56.1215718, - 3.3856475\nOrdinance Survey 10 Figure Grid Reference NT 13956 92954\nX Easting 313956 , Y Northing 692954\n\nWe hope that you have the opportunity to visit your land, and to enjoy the Scottish countryside as a\nLaird of Scotland . You can keep up to date via our Facebook page at fb.me/ScotlandTitles\n\nI very much hope that owning a piece of Scotland is something that will give you a sense of pride, and\nwould like to take this opportunity to thank you for choosing Scotland Titles`; // const page = document.getPage(0);
       const welcomeContent = `Welcome to Scotland!`; // const page = document.getPage(0);
 
@@ -198,7 +187,7 @@ export default async function handler(req, res) {
         height: pngDims.height,
       });
 
-      //Page Two of certificate
+      //Page Two of Certificate =========================================================
 
       //Image work
       const filePathTwo = path.resolve("./public", "images", "pdf-bg.jpg");
@@ -207,8 +196,31 @@ export default async function handler(req, res) {
       // console.log(imgBuffer, "imgBuffer");
       const imgBg = await pdfDoc.embedJpg(imgBufferTwo);
 
+      const filePathThree = path.resolve(
+        "./public",
+        "images",
+        "certificate-stamp.png"
+      );
+
+      const imgBufferThree = fs.readFileSync(filePathThree);
+      // console.log(imgBuffer, "imgBuffer");
+      const stampImg = await pdfDoc.embedPng(imgBufferThree);
+      const stampPngDims = stampImg.scale(0.3);
+
+      const filePathFour = path.resolve(
+        "./public",
+        "images",
+        "certificate-mid.png"
+      );
+
+      const imgBufferFour = fs.readFileSync(filePathFour);
+
+      // console.log(imgBuffer, "imgBuffer");
+      const certificateMid = await pdfDoc.embedPng(imgBufferFour);
+      const ertificateMidpngDims = certificateMid.scale(0.22);
+
       const fontTwo = fs.readFileSync(
-        path.join("./utils", "fonts", "TEMPSITC.TTF")
+        path.join("./utils", "fonts", "TempusSansITC Bold.ttf")
       );
       const tempusFont = await pdfDoc.embedFont(fontTwo);
 
@@ -220,17 +232,45 @@ export default async function handler(req, res) {
       const certficateAddress =
         "between Scotland Titles, Unit 61892, PO Box 26965, Glasgow G1 9BW United Kingdom and";
 
-      const certficateUserName = "Lady Kathryn Joy Smith ofBlairadam";
+      const certficateUserName = "Lady Kathryn Joy Smith of Blairadam";
 
       const certificateAddressTwo =
         "(hereafter to be proclaimed as “THE LORD”), care of Unit 61892, PO Box 26965, Glasgow G1 9BW United Kingdom";
+
+      const certificateText = `The Scotland Titles Estate in Fife, Scotland, hereinafter referred to as “THE ESTATE”,\nhas been partitioned into dedicated souvenir plots of land.\n
+        THE LORD has petitioned unto Scotland Titles on this day their intention to\npurchase, and Scotland Titles has determined to accept the disposition of a plot of\nland within THE ESTATE, at Cantsdam, hereafter referred to as “THE LAND”.\n
+        Scotland Titles, in CONSIDERATION of all monies due to be paid to us by THE\n LORD, of which we have received of in full, we do hereby DISCHARGE unto them\nand DISPONE to and in perpetuity in favour of THE LORD and to their future\nassignees the whole of THE LAND but always with pedestrian access only over THE\nESTATE; such rights of vehicular access are reserved to Scotland Titles and its\nsuccessors in title plus any and all others authorised by it; THE LORD covenants not\nto dispose of THE LAND in part only.\n
+        Scotland Titles is a trading name of Olgrinmore Ltd., Jersey. Terms and Conditions,\nand this CERTIFICATE shall be governed by the Law of Scotland.`;
+
+      const certificateTextTwo = `THE ESTATE location is KINGSEAT ROAD (OFF CANTSDAM ROAD),\nCANTSDAM, KELTY, FIFE, SCOTLAND KY12 0SW\n\nTHE ESTATE is recorded in the General Register of Sasines RS30-32\n\nCoordinates to the centre of THE ESTATE are;\nLatitude, Longitude in degrees 56°07${"`"}18′′N , 003°23′08′′W\nX Easting 313956 , Y Northing 692954\n\nThe Plot Number of THE LAND within THE ESTATE is 13334\n\nThe size of THE LAND is one square foot\n\nDate of Entry of THE LAND is as the date of this CERTIFICATE\n\nThis Disposition is signed for and on behalf of Scotland Titles and witnessed on the\n12th Day of June 2022`;
       pagetwo.drawImage(imgBg, {
         width: pagetwo.getWidth(),
         height: pagetwo.getHeight(),
       });
+
+      pagetwo.drawImage(certificateMid, {
+        x: 400,
+        y: 405,
+        width: ertificateMidpngDims.width,
+        height: ertificateMidpngDims.height,
+      });
+      pagetwo.drawImage(img, {
+        x: 40,
+        y: 450,
+        width: pngDims.width,
+        height: pngDims.height,
+      });
+
+      pagetwo.drawImage(stampImg, {
+        x: 700,
+        y: 50,
+        width: stampPngDims.width,
+        height: stampPngDims.height,
+      });
+
       pagetwo.drawText(certificateHeading, {
-        x: 150,
-        y: 570,
+        x: 200,
+        y: 520,
         size: 26,
         width: textWidth,
         height: textHeight,
@@ -239,8 +279,8 @@ export default async function handler(req, res) {
         font: oldEng,
       });
       pagetwo.drawText(certficateAddress, {
-        x: 170,
-        y: 550,
+        x: 220,
+        y: 500,
         width: textWidth,
         height: textHeight,
         size: 10,
@@ -251,8 +291,8 @@ export default async function handler(req, res) {
       });
 
       pagetwo.drawText(certficateUserName, {
-        x: 165,
-        y: 530,
+        x: 215,
+        y: 470,
         width: textWidth,
         height: textHeight,
         size: 24,
@@ -261,9 +301,9 @@ export default async function handler(req, res) {
         font: oldEng,
       });
 
-      pagetwo.drawText(certficateAddress, {
-        x: 145,
-        y: 510,
+      pagetwo.drawText(certificateAddressTwo, {
+        x: 195,
+        y: 450,
         width: textWidth,
         height: textHeight,
         size: 10,
@@ -271,6 +311,123 @@ export default async function handler(req, res) {
         lineHeight: fontSize * 1.2,
         // font: customFont,
         font: tempusFont,
+      });
+
+      pagetwo.drawText(certificateText, {
+        x: 50,
+        y: 370,
+        width: textWidth,
+        height: textHeight,
+        size: 10,
+        color: rgb(0, 0, 0),
+        lineHeight: fontSize * 1.2,
+        // font: customFont,
+        font: tempusFont,
+      });
+
+      pagetwo.drawText(certificateTextTwo, {
+        x: 450,
+        y: 370,
+        width: textWidth,
+        height: textHeight,
+        size: 10,
+        color: rgb(0, 0, 0),
+        lineHeight: fontSize * 1.2,
+        // font: customFont,
+        font: tempusFont,
+      });
+
+      //Page Three of Deed of certificate =========================================================
+
+      const MainHeading = "Master Title Deed";
+      const SubHeading = "Deed of Change of Name and Title (Deed Poll)";
+      const formerTitle = "(Former Name & Title)";
+      const newTitle = "(New Name & Title)";
+
+      // const underlineHeight = 1;
+      // const underlineY = textHeight - fontSize;
+      // const underlineX1 = 100; // Adjust the starting x-position of the underline
+      // const underlineX2 = 250; // Adjust the ending x-position of the underline
+      const underlineHeight = 1;
+      const underlineY = 680; // Adjust the y-position as needed
+
+      // Adjust the x-positions as needed for each form field
+      const underlineX1 = 170; // For Form Field 1
+      const underlineX2 = 270; // For Form Field 1
+      const underlineX3 = 170; // For Form Field 2
+      const underlineX4 = 270; // For Form Field 2
+      const underlineX5 = 90; // For Form Field 3
+      const underlineX6 = 580; // For Form Field 3
+
+      deedPage.drawLine({
+        start: { x: underlineX5, y: 600 }, // Adjust the y-position for Form Field 3
+        end: { x: underlineX6, y: 600 }, // Adjust the y-position for Form Field 3
+        color: rgb(0, 0, 0),
+        thickness: underlineHeight,
+      });
+      const deedFormText = `of Craig Edward Smith ${formerTitle}\n\nnow Lord Craig Edward Smith ${newTitle}\n\nBY THIS DEED OF CHANGE OF NAME AND TITLE made by myself the undersigned\n\nLord Craig Edward Smith,\n\nof`;
+      // deedPage.drawLine({
+      //   x: 70,
+      //   y: 680,
+      //   // start: { x: underlineX1, y: underlineY },
+      //   // end: { x: underlineX2, y: underlineY },
+      //   color: rgb(0, 0, 0),
+      //   thickness: underlineHeight,
+      // });
+      deedPage.drawText(MainHeading, {
+        x: 200,
+        y: 750,
+        size: 20,
+        width: textWidth,
+        height: textHeight,
+        color: rgb(0, 0, 0),
+        lineHeight: fontSize * 1.2,
+        font: timesRomanFontHeading,
+      });
+      deedPage.drawText(SubHeading, {
+        x: 180,
+        y: 730,
+        width: textWidth,
+        height: textHeight,
+        size: 12,
+        color: rgb(0, 0, 0),
+        lineHeight: fontSize * 1.2,
+        // font: customFont,
+        font: timesRomanFont,
+      });
+
+      deedPage.drawText(deedFormText, {
+        x: 30,
+        y: 710,
+        width: textWidth,
+        height: textHeight,
+        size: 12,
+        color: rgb(0, 0, 0),
+        lineHeight: fontSize * 1.2,
+        // font: customFont,
+        font: timesRomanFont,
+      });
+
+      // deedPage.drawText(content, {
+      //   x: 50,
+      //   y: 600,
+      //   width: textWidth,
+      //   height: textHeight,
+      //   size: fontSize,
+      //   color: rgb(0, 0, 0),
+      //   lineHeight: fontSize * 1.2,
+      //   font: timesRomanFont,
+      // });
+
+      deedPage.drawText(welcomeContent, {
+        x: 170,
+        y: 300,
+        width: textWidth,
+        height: textHeight,
+        size: fontSize,
+        color: rgb(0, 0, 0),
+        lineHeight: fontSize * 1.2,
+        font: timesRomanFont,
       });
 
       const pdfBytes = await pdfDoc.save();
@@ -293,8 +450,8 @@ export default async function handler(req, res) {
       res.status(500).send("An error occurred");
     }
   };
-  //   onlyLord();
-  //   return;
+  onlyLord();
+  return;
   if (email && req.body.line_items.length > 0) {
     req.body.line_items.map((item) => {
       // if(product)
