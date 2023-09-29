6a1429ff95bf61cbc168567938f305f4f146d427
Yasir Iqbal <yasirupworkdev@gmail.com>
2023-09-29 18:47:43 +0500

    Update hello.js

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -5,6 +5,8 @@ import connectToDatabase from "../../../db";
 import path from "path";
 // import { connectToDatabase } from '../../path-to-mongodb.js';
 import fs from "fs";
+import { degrees, PDFDocument, rgb, StandardFonts } from "pdf-lib";
+import fontkit from "@pdf-lib/fontkit";
 
 export default async function handler(req, res) {
   const db = await connectToDatabase();
@@ -12,11 +14,29 @@ export default async function handler(req, res) {
   if (req.method === "GET") {
     console.log(path.join(process.cwd(), "public", "Goudy-Bold-Regular.ttf"));
     // path.join(process.cwd(), "public", "demo.json")
+    const pdfDoc = await PDFDocument.create();
+    pdfDoc.registerFontkit(fontkit);
+
     const fontTwo = fs.readFileSync(
       // path.join(__dirname + "../../../../utils/fonts/demo.json")
       // "https://scotlandtitlesapp.com/pdfs/Goudy-Bold-Regular.ttf"
       path.join(process.cwd(), "public", "Goudy-Bold-Regular.ttf")
     );
+    const tempusFont = await pdfDoc.embedFont(fontTwo);
+    var page = pdfDoc.addPage([595, 842]);
+
+    page.drawText("abcdefg", {
+      x: 70,
+      y: 710,
+      size: 11,
+      // width: textWidth,
+      // height: textHeight,
+      color: rgb(0, 0, 0),
+      // lineHeight: fontSize * 1.2,
+      font: tempusFont,
+    });
+    const pdfBytes = await pdfDoc.save();
+    console.log(pdfBytes, "pdfBytes");
     // const collection = db.collection('co'); // Replace with your collection name
     // const data = await collection.find({}).toArray();
     // res.status(200).json(data);
