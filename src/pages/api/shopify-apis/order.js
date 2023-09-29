8f119ba78a73be1ef0ce34f8ff0e68daa79defae
Yasir Iqbal <yasirupworkdev@gmail.com>
2023-09-29 18:19:32 +0500

    change

--- src/pages/api/shopify-apis/order.js
+++ src/pages/api/shopify-apis/order.js
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
     StandardFonts.TimesRomanItalic
   );
   const fontTwo = fs.readFileSync(
-    path.join(__dirname + "../../../utils/fonts/OLDENGL.TTF")
+    path.join(__dirname + "../../../../utils/fonts/OLDENGL.TTF")
     // "https://scotlandtitlesapp.com/pdfs/Goudy-Bold-Regular.ttf"
     // path.join(process.cwd(), "public", "Goudy-Bold-Regular.ttf")
   );
