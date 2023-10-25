85d736e64f9bb0cb4156804b9e2dd905f1667236
Yasir Iqbal <yasirupworkdev@gmail.com>
2023-10-25 17:26:58 +0500

    title pack deed name two capital

--- src/pages/api/shopify-apis/order.js
+++ src/pages/api/shopify-apis/order.js
@@ -866,7 +866,26 @@ export default async function handler(req, res) {
         });
       });
 
-      const deedUserNameWidth = `of ${propObject.p_8727183196433._Name1}`;
+      //Capital name
+      let deedNameParts = propObject.p_8727183196433._Name1.split(" ");
+      console.log(nameParts, "nameParts");
+      let deedModifiedName = deedNameParts
+        .map(
+          (part) => part.charAt(0).toUpperCase() + part.slice(1).toLowerCase()
+        )
+        .join(" ");
+
+      //two name
+
+      let deedNamePartsTwo = propObject.p_8727183196433._Name2.split(" ");
+      let deedModifiedNameTwo = deedNamePartsTwo
+        .map(
+          (part) => part.charAt(0).toUpperCase() + part.slice(1).toLowerCase()
+        )
+        .join(" ");
+      console.log(deedModifiedNameTwo, "modified name two");
+
+      const deedUserNameWidth = `of ${deedModifiedName}`;
       const formertextWidth = timesRomanFontHeading.widthOfTextAtSize(
         deedUserNameWidth,
         12
@@ -874,7 +893,7 @@ export default async function handler(req, res) {
       console.log(formertextWidth, "formertextWidth");
       const totalWidth = formertextWidth + 35;
 
-      const deedNewNameWidth = `now ${propObject.p_8727183196433._Title1} ${propObject.p_8727183196433._Name1}`;
+      const deedNewNameWidth = `now ${propObject.p_8727183196433._Title1} ${deedModifiedName}`;
       const newtextWidth = timesRomanFontHeading.widthOfTextAtSize(
         deedNewNameWidth,
         12
@@ -915,7 +934,7 @@ export default async function handler(req, res) {
         font: timesRomanFontHeading,
       });
 
-      const deedFormText = `of ${propObject.p_8727183196433._Name1}\n\nnow ${propObject.p_8727183196433._Title1} ${propObject.p_8727183196433._Name1}\n\nBY THIS DEED OF CHANGE OF NAME AND TITLE made by myself the undersigned\n\n${propObject.p_8727183196433._Title1} ${propObject.p_8727183196433._Name1}\n\nof`;
+      const deedFormText = `of ${deedModifiedName}\n\nnow ${propObject.p_8727183196433._Title1} ${deedModifiedName}\n\nBY THIS DEED OF CHANGE OF NAME AND TITLE made by myself the undersigned\n\n${propObject.p_8727183196433._Title1} ${deedModifiedName}\n\nof`;
       const declarationOne = `HEREBY DECLARE AS FOLLOWS;`;
       const absolute =
         "1.   I ABSOLUTELY and entirely renounce, relinquish and abandon the use of my said";
@@ -945,8 +964,8 @@ export default async function handler(req, res) {
       const yearIn = "IN THE YEAR";
       const signedAs = "SIGNED AS A DEED AND DELIVERED\n\nby the above named";
       const signPlaceHolder = "Signature";
-      const lordName = `${propObject.p_8727183196433._Title1} ${propObject.p_8727183196433._Name1}\n\nFormerly known as`;
-      const formerName = `${propObject.p_8727183196433._Name1}`;
+      const lordName = `${propObject.p_8727183196433._Title1} ${deedModifiedName}\n\nFormerly known as`;
+      const formerName = `${deedModifiedName}`;
 
       const presence = "In the presence of:";
       const witness = "Witness's signature";
@@ -1493,7 +1512,25 @@ export default async function handler(req, res) {
           });
         });
 
-        const deedTwoUserNameWidth = `of ${propObject.p_8727183196433._Name2}`;
+        //capital name for second
+        let deedNameParts = propObject.p_8727183196433._Name1.split(" ");
+        console.log(nameParts, "nameParts");
+        let deedModifiedName = deedNameParts
+          .map(
+            (part) => part.charAt(0).toUpperCase() + part.slice(1).toLowerCase()
+          )
+          .join(" ");
+
+        //two name
+
+        let deedNamePartsTwo = propObject.p_8727183196433._Name2.split(" ");
+        let deedModifiedNameTwo = deedNamePartsTwo
+          .map(
+            (part) => part.charAt(0).toUpperCase() + part.slice(1).toLowerCase()
+          )
+          .join(" ");
+        console.log(deedModifiedNameTwo, "modified name two");
+        const deedTwoUserNameWidth = `of ${deedModifiedNameTwo}`;
         const formerDeedTwotextWidth = timesRomanFontHeading.widthOfTextAtSize(
           deedTwoUserNameWidth,
           12
@@ -1501,7 +1538,7 @@ export default async function handler(req, res) {
         // console.log(formertextWidth, "formertextWidth");
         const totalWidthDeedTwo = formerDeedTwotextWidth + 35;
 
-        const deedTwoNewNameWidth = `now ${propObject.p_8727183196433._Title2} ${propObject.p_8727183196433._Name2}`;
+        const deedTwoNewNameWidth = `now ${propObject.p_8727183196433._Title2} ${deedModifiedNameTwo}`;
         const newDeedTwotextWidth = timesRomanFontHeading.widthOfTextAtSize(
           deedTwoNewNameWidth,
           12
@@ -1542,10 +1579,10 @@ export default async function handler(req, res) {
           font: timesRomanFontHeading,
         });
 
-        const deedFormTextTwo = `of ${propObject.p_8727183196433._Name2}\n\nnow ${propObject.p_8727183196433._Title2} ${propObject.p_8727183196433._Name2}\n\nBY THIS DEED OF CHANGE OF NAME AND TITLE made by myself the undersigned\n\n${propObject.p_8727183196433._Title2} ${propObject.p_8727183196433._Name2}\n\nof`;
+        const deedFormTextTwo = `of ${deedModifiedNameTwo}\n\nnow ${propObject.p_8727183196433._Title2} ${deedModifiedNameTwo}\n\nBY THIS DEED OF CHANGE OF NAME AND TITLE made by myself the undersigned\n\n${propObject.p_8727183196433._Title2} ${deedModifiedNameTwo}\n\nof`;
 
-        const lordNameTwo = `${propObject.p_8727183196433._Title2} ${propObject.p_8727183196433._Name2}\n\nFormerly known as`;
-        const formerNameTwo = `${propObject.p_8727183196433._Name2}`;
+        const lordNameTwo = `${propObject.p_8727183196433._Title2} ${deedModifiedNameTwo}\n\nFormerly known as`;
+        const formerNameTwo = `${deedModifiedNameTwo}`;
 
         deedPageTwo.drawText(MainHeading, {
           x: 200,
