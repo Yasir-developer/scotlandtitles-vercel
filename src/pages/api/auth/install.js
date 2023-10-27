a5f7f5915dacecc8d3493f9e99b7c5e6f29c47e2
Yasir Iqbal <yasirupworkdev@gmail.com>
2023-10-28 00:52:30 +0500

    CHANGES

--- src/pages/api/auth/install.js
+++ src/pages/api/auth/install.js
@@ -16,5 +16,6 @@ export default async function handler(req, res) {
   }
   const installUrl = `https://${shop}/admin/oauth/authorize?client_id=${client_id}&scope=read_products,write_products,read_themes,write_themes,read_orders,write_orders,read_checkouts,write_checkouts,read_content,write_content,read_customers,write_customers&redirect_uri=https://${process.env.APP_URL}/api/auth/callback`;
   console.log(installUrl);
+  res.status(200).json({ message: installUrl });
   // res.redirect(installUrl);
 }
