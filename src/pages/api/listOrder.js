d6e348adfe61716ce8ada0e7c86df73bcf2327af
Yasir Iqbal <yasirupworkdev@gmail.com>
2023-11-27 17:27:24 +0500

    Update listOrder.js

--- src/pages/api/listOrder.js
+++ src/pages/api/listOrder.js
@@ -18,12 +18,16 @@ export default async function listOrder(req, res) {
     };
     //scotland-titles-app-development.myshopify.com
     console.log(req.query, "req.quert");
-    const url = `https://scotland-titles-dev.myshopify.com/admin/api/2023-10/orders.json${
-      req.query.page_info ? `?page_info=${req.query.page_info}` : ""
-    }${req.query.name ? `?name=${req.query.name}` : ""}`;
 
+    const url = `https://scotland-titles-dev.myshopify.com/admin/api/2023-10/orders.json${
+      req.query.name
+        ? `?name=${req.query.name}`
+        : req.query.page_info
+        ? `?page_info=${req.query.page_info}`
+        : `?status=any`
+    }`;
     console.log(url, "all url");
-    console.log(accessToken.accessToken, "accessToken from orderlist");
+    // console.log(accessToken.accessToken, "accessToken from orderlist");
     await axios
       .get(url, {
         headers: {
@@ -43,34 +47,4 @@ export default async function listOrder(req, res) {
   } catch (error) {
     return res.json({ dbo: error });
   }
-  // console.log(access_token.accessToken, "accessToken console");
-  // var result;
-  // const options = {
-  //   headers: {
-  //     "Access-Control-Allow-Origin": "*",
-  //     "X-Shopify-Access-Token": "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",
-  //   },
-  // };
-  // console.log(req.query, "req.quert");
-  // const url = `https://scotland-titles-app-development.myshopify.com/admin/api/2023-10/orders.json${
-  //   req.query.page_info ? `?page_info=${req.query.page_info}` : ""
-  // }${req.query.name ? `?name=${req.query.name}` : ""}`;
-
-  // console.log(url, "all url");
-  // await axios
-  //   .get(url, {
-  //     headers: {
-  //       "Access-Control-Allow-Origin": "*",
-  //       "X-Shopify-Access-Token": "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",
-  //     },
-  //   })
-  //   .then((response) => {
-  //     // console.log(response.data.orders, "total respons");
-  //     result = { data: response.data.orders, headers: response.headers };
-  //   })
-  //   .catch((error) => {
-  //     console.log(error.message, "error");
-  //   });
-  // return res.json({ data: result });
-  // return res.json({ dbo: orders });
 }
