121ddf055cd4e3686d2fbc3ef88bf8dd7772f0db
Yasir Iqbal <yasirupworkdev@gmail.com>
2023-11-30 19:24:41 +0500

    discounted key

--- src/pages/api/shopify-apis/order.js
+++ src/pages/api/shopify-apis/order.js
@@ -17436,7 +17436,9 @@ export default async function handler(req, res) {
                 _Title1: resultObjectdiscountEmblemPack._Title1,
                 _Name1: resultObjectdiscountEmblemPack._Name1,
                 _Title2: resultObjectdiscountEmblemPack._Title2,
-                _Name2: resultObjectdiscountEmblemPack._Name2,
+                _Name2: resultObjectdiscountEmblemPack._name2
+                  ? resultObjectdiscountEmblemPack._name2
+                  : resultObjectdiscountEmblemPack._Name2,
                 variant:
                   pProperties[`p_6882555658426_${discountEmblemIncrement}`]
                     .variant_title,
