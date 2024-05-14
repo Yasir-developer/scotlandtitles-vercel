7559e9f846427413628b9c2ca369cffb826cac22
Yasir <yasirupworkdev@gmail.com>
2024-05-14 21:06:29 +0500

    Update order.js

--- src/pages/api/shopify-apis/order.js
+++ src/pages/api/shopify-apis/order.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
   // console.log(req.method, "req");
   const { id, email, created_at, order_number } = req.body;
   console.log(req.body, "req.body");
-  // const { first_name, last_name } = req.body.customer;
+  const { first_name, last_name } = req.body.customer;
 
   const titlePackId = 6846298849466;
   const emblemId = 6846299078842;
